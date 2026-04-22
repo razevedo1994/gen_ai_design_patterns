30c96a199247da0ecc4ab25739579fb5a88da1f9
Rodrigo <razevedo.contato@gmail.com>
2026-04-22 00:03:41 -0300

    rename folder

--- 00_introduction/runing_with_ollama.py
+++
@@ -1,8 +0,0 @@
-from pydantic_ai.models.openai import OpenAIChatModel
-from pydantic_ai.providers.openai import OpenAIProvider
-
-
-model = OpenAIChatModel(
-    model_name="llama3.2",
-    provider=OpenAIProvider(base_url="http://localhost")
-)
